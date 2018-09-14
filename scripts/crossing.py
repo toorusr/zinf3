7584594a5f4eee9103302a66c557f78f3de2fccd
Toorusr <toorusr@protonmail.com>
2018-09-14 22:19:04 +0200

    Add: minimal changes

--- scripts/crossing.py
+++ scripts/crossing.py
@@ -16,12 +16,12 @@ canvas = street.init(width=1920, heigth=1080) # initializing a street canvas wit
 canvas.background(scene="pretty_street") # changing the canvas enviroment
 canvas.title("Trafic light simulation") # changing the title of the window
 
-class Crossing:
+class TraficLights:
     # init function: create objects, start loop
     def __init__(self, start_value):
         self.traf0 = canvas.create.traficlight(x=500, y=300) # Creating the car trafic light traf0
         self.traf1 = canvas.create.traficlight(x=300, y=500) # Creating the people trafic light traf1
-        self.loop(start_value) # Starting the loop
+        self.traf0_state, self.traf1_state = [2, 0] # assign start states
 
     # change function: change trafic lights visual output
     def change(self, state, trafic_light):
@@ -29,23 +29,29 @@ class Crossing:
         if state in [0,1] and trafic_light in [0,1]: # check if state and trafic_light have valide values
             if trafic_light == 0: # check if travic_light = 0 -> self.traf0
                 self.traf0.state(state_colors[state]) # change visual state of traficlight 0 by getting color name by index
+                self.traf0_state = state # change current state var
             else: # else change self.traf1
                 self.traf1.state(state_colors[state]) # change visual state of traficlight 1 by getting color name by index
+                self.traf1_state = state # change current state var
         else:
             return 1 # return that something went wrong
 
     # transition function: smooth change of values of traficlights
     def transition():
+        pass
 
     # get function: get current value of traficlight
     def get(self, traf):
         pass
-        
-    # loop function: traficlight cycle
-    def loop(self):
-        self.change(2, 0) # set start value of car traficlight to red
-        self.change(0, 1) # set start value of people traficlight to green
-        while True: # starting a infinite loop
-            self.change()
 
-cross = Crossing() # initializing the crossing
+    # start function: traficlight cycle loop
+    def start(self):
+        self.change(state=self.traf0_state, trafic_light=0) # change visual to start value of car traficlight
+        self.change(state=self.traf1_state, trafic_light=1) # change visual to start value of people traficlight
+        while True: # starting a infinite loop
+            self.transition()
+
+class
+
+traf = TraficLights() # initializing the traficlights
+traf.start()
